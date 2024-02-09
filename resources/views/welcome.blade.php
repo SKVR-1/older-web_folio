36aefbef3a3c03757ae0e8e859ce0005997cc619
Sud <sud@kirens-iMac.local>
2024-02-09 20:04:14 +0000

    js fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        @vite('resources/js/script.js')
+        <!-- @vite('resources/js/script.js') -->
         @vite('resources/js/app.js')
         @vite('resources/css/app.css')
         <title>SVKR::Portfolio</title>
